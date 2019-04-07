a19281b1a55eb6341acb7d8566dd06f0b52cf315
AmauriMarcos <amaurisantos.m@gmail.com>
2019-04-07 12:48:20 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const bodyParser = require('body-parser'),
+      mongoose   = require('mongoose'),
+      ejs        = require('ejs'),
+      express    = require('express'),
+      app        = express();
+
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
+
+mongoose.connect('mongodb://localhost:27017/onePage', {useNewUrlParser: true});
+
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+
+
+
+
+app.listen(3000, () => console.log('Connected on port 3000!!'));
