9683f794df3cfe04eeb5aa54317c30568c87576a
AmauriMarcos <amaurisantos.m@gmail.com>
2019-04-09 15:40:12 -0300

    Drop database

--- app.js
+++ app.js
@@ -11,12 +11,49 @@ app.use(express.static('public'));
 
 mongoose.connect('mongodb://localhost:27017/onePage', {useNewUrlParser: true});
 
+const quizSchema = new mongoose.Schema(
+    {
+        question: String,
+        answers : [
+            {first: String},
+            {second: String},
+            {third: String}
+        ]
+    }
+);
+
+const Quiz = mongoose.model('Quiz', quizSchema);
+
+// Quiz.create({question: 'Onde tem mais mosquitos?'},
+//            {answers: [{first :'Chile'},
+//                       {second:'Sérvia'}, 
+//                        {third:'Brasil'}] }, (err) => {
+//                            err ? console.log(err) : console.log('Succesfully added to onePageDB');
+//                        });
+
+
+
+
 
 app.get('/', (req, res) => {
     res.render('home');
 });
 
 
+app.get('/nivelamento', (req, res) => {
+    Quiz.find((err, allQuizzes) => {
+        if (err){
+            console.log(err);
+        } else {  
+            for(var i = 0; i < allQuizzes.length; i++){
+                const answer  = allQuizzes[i][1];
+                console.log(answer);
+            };                       
+            
 
+           res.render('nivelamento', {questions: allQuizzes});
+        };
+    });
+});
 
 app.listen(3000, () => console.log('Connected on port 3000!!'));
