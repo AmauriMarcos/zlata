f92483789ade7a4d0a41e6b6c39c552262a8cee2
AmauriMarcos <amaurisantos.m@gmail.com>
2019-04-09 16:31:28 -0300

    without database

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const bodyParser = require('body-parser'),
-      mongoose   = require('mongoose'),
       ejs        = require('ejs'),
       express    = require('express'),
       app        = express();
@@ -9,32 +8,6 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
 
-mongoose.connect('mongodb://localhost:27017/onePage', {useNewUrlParser: true});
-
-const quizSchema = new mongoose.Schema(
-    {
-        question: String,
-        firstOption : String,
-        secondOption : String,
-        thirdOption : String
-    }
-);
-
-const Quiz = mongoose.model('Quiz', quizSchema);
-
-// Quiz.create({question: 'When do you study?', 
-//               firstOption:'at school',
-//               secondOption:'in the evenings',
-//               thirdOption:'in the library'}, (err) => {
-//                 if(err){
-//                     console.log(err);
-//                 }else{
-//                     console.log('Succesfully created a new question!');
-//                 }
-//  });
-
-
-
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -42,14 +15,9 @@ app.get('/', (req, res) => {
 
 
 app.get('/nivelamento', (req, res) => {
-    Quiz.find((err, allQuizzes) => {
-        if (err){
-            console.log(err);
-        } else {  
-
-           res.render('nivelamento', {allQuizzes: allQuizzes});
-        };
-    });
+  
+    res.render('nivelamento');
+    
 });
 
 app.listen(3000, () => console.log('Connected on port 3000!!'));
