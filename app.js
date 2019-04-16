f9cd9eb4457966e10e43a36acba0a49db3a4b85a
AmauriMarcos <amaurisantos.m@gmail.com>
2019-04-16 09:53:43 -0300

    finish newsletter page and create ADK folder

--- app.js
+++ app.js
@@ -1,57 +1,79 @@
 const bodyParser = require('body-parser'),
       mongoose   = require('mongoose'),
+      request    = require('request'),
       ejs        = require('ejs'),
-      quizVariables = require('./quiz.js'),
-      questions  = require('./quiz.js'),
       express    = require('express'),
       app        = express();
 
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
+// app.use(express.static(path.join(__dirname, 'public')))
 
-mongoose.connect('mongodb://localhost:27017/onePage', {useNewUrlParser: true});
-
-var opcaoEscolhida;
-
-const quizSchema = new mongoose.Schema(
-    {
-        question: String,
-        firstOption : String,
-        secondOption : String,
-        thirdOption : String,
-        rightAnswer : String
-    }
-);
-
-const Quiz = mongoose.model('Quiz', quizSchema);
 
 
 app.get('/', (req, res) => {
     res.render('home');
 });
 
-
-app.get('/nivelamento', (req, res) => {
-     Quiz.find((err, data) => {
-         
-        err ? console.log (err) : res.render('nivelamento', {quizzes:data, opcaoEscolhida: opcaoEscolhida});
-     });
-      
+app.get('/nivelamento', (req, res) =>{
+    res.render('nivelamento');
 });
 
-app.post('/nivelamento', (req, res) =>{
-    opcaoEscolhida = req.body.options;
-    Quiz.find((err) => {
-        if(err){
-            console.log(err);
-        } else {
-            res.redirect('nivelamento');
+app.get('/newsletter',(req, res) =>{
+    res.render('newsletter')
+});
+
+app.post('/newsletter', (req, res) =>{
+    const news = req.body.news;
+    console.log(news.email);
+
+    const data = {
+        members: [
+            {
+                email_address: news.email,
+                status: 'subscribed',
+                merge_fields: {
+                    FNAME: news.nome,
+                    LNAME: news.sobrenome
+                }
+
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const options = {
+        url: 'https://us20.api.mailchimp.com/3.0/lists/14ef3243b9',
+        method: 'POST',
+        headers: {
+            'Authorization': 'zlata 9d62f9b32fd6b90b686110584073b911-us20'
+        },
+        body: jsonData
+    };
+
+    request(options, (error, response, body) =>{
+        if(error){
+            res.sendFile(__dirname + "/failure.html")
+            console.log(response.statusCode);
+        } else{
+            if(response.statusCode === 200){
+                console.log(response.statusCode);
+                res.sendFile(__dirname + "/success.html")
+            } else{
+                res.sendFile(__dirname + "/failure.html")
+            }            
         }
-     });    
+    });
 });
 
-app.post('')
+app.post('/failure', function(req, res){
+    res.redirect('/')
+})
+
 
 app.listen(3000, () => console.log('Connected on port 3000!!'));
+
+//	1Scua7qAsWSF8DZxm9uSjh2JqLEUeMGi //Tomtom API Key
---
+++ public/quiz.js
@@ -0,0 +1,92 @@
+const quizArray = [];
+var pos = 0;
+var correto = 0; 
+var pergunta; 
+var op1; 
+var op2; 
+var op3; 
+var resp;
+
+const Quiz = function(question, opcao1, opcao2, opcao3, resposta){
+     this.question = question;
+     this.opcao1   = opcao1;
+     this.opcao2   = opcao2;
+     this.opcao3   = opcao3;
+     this.resposta = resposta;
+}
+
+const primeira = new Quiz('When can we meet again?', "When are you free?", "It was two days ago.", "Can you help me?", 'A');
+const segunda  = new Quiz('My aunt is going to stay with me.', "How do you do?", "How long for?", "How was it?", 'B');
+const terceira = new Quiz('When do you study?', "at school", "in the evenings", "in the library", 'B');
+const quarta   = new Quiz('Would you prefer lemonade or orange juice?', "Have you got anything else?", "If you like.", "Are you sure about that?", 'A');
+const quinta   = new Quiz("Let's have dinner now.", "You aren't eating.", "There aren't any.", "Tom isn't here yet", 'C');
+const sexta    = new Quiz("The snow was ...... heavily when I left the house.", "dropping", "landing", "falling", 'C');
+const setima   = new Quiz("I can't find my keys anywhere - I ...... have left them at work.", "can", "must", "would", 'B');
+const oitava   = new Quiz("When a car pulled out in front of her, Jane did well not to ...... control of her bicycle.", "miss", "lose", "drop", 'B');
+const nona     = new Quiz("According to Richard's ...... the train leaves at 7 o'clock.", "information", "knowledge", "opinion", 'A');
+const decima   = new Quiz("When you stay in a country for some time you get used to the people's ...... of life.", "habit", "way", "system", 'B');
+const decima1  = new Quiz("The builders are ...... good progress with the new house.", "getting", "doing", "making", 'C');
+const decima2  = new Quiz("She is now taking a more positive ...... to her studies and should do well.", "attitude", "behaviour", "style", 'A');
+const decima3  = new Quiz("My father ...... his new car for two weeks now.", "has had", "has", "is having", 'A');
+const decima4  = new Quiz("What differences are there ...... the English spoken in the UK and the English spoken in the US?", "among", "between", "with", 'B');
+const decima5  = new Quiz("At 6 p.m. I started to get angry with him because he was late ......", "as usual.", "typically.", "usually.", 'A');
+const decima6  = new Quiz("...... you get your father's permission, I'll take you skiing next weekend.", "Provided", "As", "Unless", 'B');
+const decima7  = new Quiz("A local company has agreed to ...... the school team with football shirts.", "supply", "give", "produce", 'A');
+const decima8  = new Quiz("I really enjoy stories that are ...... in the distant future.", "placed", "put", "set", 'A');
+const decima9  = new Quiz("That old saucepan will come in ...... when we go camping.", "fitting", "convenient", "suitable", 'B');
+const vigesima = new Quiz("Anyone ...... after the start of the play is not allowed in until the interval.", "arrives", "arriving", "arrived", 'B');
+
+quizArray.push(primeira, segunda, terceira, quarta, quinta, sexta, setima, oitava,
+    nona, decima, decima1, decima2, decima3, decima4, decima5, decima6, decima7,
+    decima8, decima9, vigesima);
+
+
+function seleciona(x){
+    return document.getElementById(x);
+};
+
+function criaQuestao() {
+    var myDiv = seleciona('teste');
+    var h6 = seleciona('progresso');
+
+    if(pos >= quizArray.length){
+        myDiv.innerHTML = "<h2>Você acertou " + correto + ' de ' + quizArray.length + " questões</h2>"
+        myDiv.innerHTML +=  "<button onClick='document.location.reload(true)' class='btn btn-danger btn-sm'>Refazer</button>";
+        pos = 0;
+        correto = 0;
+        return false;
+    }
+
+    h6.innerHTML = 'Questão ' + (pos + 1) + ' de ' + quizArray.length;
+   
+    pergunta = quizArray[pos].question;
+    op1      = quizArray[pos].opcao1;
+    op2      = quizArray[pos].opcao2;   
+    op3      = quizArray[pos].opcao3; 
+        
+    myDiv.innerHTML = "<h2>" + pergunta + "</h2><br>";
+    myDiv.innerHTML += "<input type='radio' name='opcoes' value='A'> " + op1 + "<br>";
+    myDiv.innerHTML += "<input type='radio' name='opcoes' value='B'> " + op2 + '<br>';
+    myDiv.innerHTML += "<input type='radio' name='opcoes' value='C'> " + op3 + '<br><br>';
+    myDiv.innerHTML +=  "<button onclick='verificaResposta()' class='btn btn-danger btn-sm'>Submit Answer</button>";
+
+
+};
+
+function verificaResposta(){
+    var opcoes = document.getElementsByName('opcoes');
+    for(var i = 0; i < opcoes.length; i++){
+        if(opcoes[i].checked){
+            opcoes = opcoes[i].value;
+        } 
+    }
+
+    if(opcoes === quizArray[pos].resposta){
+        correto++;
+    }
+
+    pos++;
+    criaQuestao();
+};
+
+window.addEventListener('load', criaQuestao, false);
