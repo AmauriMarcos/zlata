a41fcf7989140c9411ad3c9ac167652b79d87131
AmauriMarcos <amaurisantos.m@gmail.com>
2019-05-07 09:28:02 -0300

    add favicon.ico at public

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const nodeMailer = require('nodemailer');
 app.set('view engine', 'ejs');
 mongoose.set('useCreateIndex', true);
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public','favicon.ico'));
 app.use(cookieParser());
 
 app.use(indexRoute);
